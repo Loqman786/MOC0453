352af0bd8a66af7bb0d4901ac1c2f1fd030dbb5b
Loqman786 <91545135+Loqman786@users.noreply.github.com>
2022-03-08 14:15:45 +0000

    app1

--- ConsoleAppProject/App02/BMI.cs
+++ ConsoleAppProject/App02/BMI.cs
@@ -1,117 +1,94 @@
 ﻿namespace ConsoleAppProject.App02
 {
     /// <summary>
-    /// Please describe the main features of this App
+    /// This app will allow student to enter their weight in 
     /// </summary>
-    /// <author> Mohammed Loqman
-    /// Student Name version 0.1
+    /// <author>
+    /// Mohammed Loqman version 0.1
     /// </author>
     public class BMI
     {
-        public void Run()
+        public const string METRIC = "METRIC";
+        public const string IMPERIAL = "IMPERIAL";
 
+        public double weight;
+        public double height;
+
+        public string SelectedUnit;
+
+        public double bmiResult = 0;
+
+        public string[] MenuChoices = { METRIC, IMPERIAL };
+
+        public void OutputUnits()
         {
-
-            using System;
-
-namespace ConsoleAppProject.App02
-    {
-        /// <summary>
-        /// Please describe the main features of this App
-        /// </summary>
-        /// <author> Mohammed Loqman
-        /// Student Name version 0.1
-        /// </author>
-        public class BMI
-        {
-            public const string METRIC = "METRIC";
-            public const string IMPERIAL = "IMPERIAL";
-
-            public double weight;
-            public double height;
-
-            public string SelectedUnit;
-
-            public double bmiResult = 0;
-
-            public string[] MenuChoices = { METRIC, IMPERIAL };
-
-            public void OutputUnits()
-            {
-                Console.WriteLine("Please choose between: ");
-                ConsoleHelper.OutputMenu(MenuChoices);
-            }
-            public string GetUnit()
-            {
-                SelectedUnit = Console.ReadLine().ToUpper();
-                return SelectedUnit;
-            }
-
-            public double GetWeight()
-            {
-                if (SelectedUnit == METRIC)
-                {
-                    Console.WriteLine("Please enter the weight in KGs:");
-                }
-                else
-                {
-                    Console.WriteLine("Please enter the weight in Stones:");
-                    Console.WriteLine("Please enter the weight in Pounds:");
-                }
-                weight = Convert.ToDouble(Console.ReadLine());
-                return weight;
-            }
-
-            public double GetHeight()
-            {
-                if (SelectedUnit == METRIC)
-                {
-                    Console.WriteLine("Please enter the height in CMs:");
-                }
-                else
-                {
-                    Console.WriteLine("Please enter the height in Feet:");
-                    Console.WriteLine("Please enter the height in Inches:");
-                }
-                height = Convert.ToDouble(Console.ReadLine());
-                return height;
-            }
-
-            public void CalculateBMI()
-            {
-                if (SelectedUnit == METRIC)
-                {
-                    bmiResult = (weight / height / height) * 10000;
-                }
-                bmiResult = Math.Floor(bmiResult);
-            }
-
-            public string GetBMICategory()
-            {
-                if (bmiResult < 18.5)
-                    return "Underweight";
-                else if (bmiResult <= 24.9)
-                    return "Normal";
-                else if (bmiResult <= 29.9)
-                    return "Overweight";
-                else if (bmiResult <= 34.9)
-                    return "Obese";
-                return "Not Yet Determined...";
-            }
-
-            public void OutputResult()
-            {
-                Console.WriteLine(SelectedUnit + " " + bmiResult + " " + GetBMICategory());
-            }
-
-
-
+            Console.WriteLine("Please choose between: ");
+            ConsoleHelper.OutputMenu(MenuChoices);
         }
-    }
+        public string GetUnit()
+        {
+            SelectedUnit = System.Console.ReadLine().ToUpper();
+            return SelectedUnit;
+        }
+
+        public double GetWeight()
+        {
+            if (SelectedUnit == METRIC)
+            {
+                System.Console.WriteLine("Please enter the weight in kgs:");
+            }
+            else
+            {
+                System.Console.WriteLine("enter the weight in Stones:");
+                System.Console.WriteLine("enter the weight in Pounds:");
+            }
+            weight = System.Convert.ToDouble(System.Console.ReadLine());
+            return weight;
+        }
+
+        public double GetHeight()
+        {
+            if (SelectedUnit == METRIC)
+            {
+                System.Console.WriteLine("Please enter the height in cms:");
+            }
+            else
+            {
+                System.Console.WriteLine("Please enter the height in Feet:");
+                System.Console.WriteLine("Please enter the height in Inches:");
+            }
+            height = System.Convert.ToDouble(System.Console.ReadLine());
+            return height;
+        }
+
+        public void CalculateBMI()
+        {
+            if (SelectedUnit == METRIC)
+            {
+                bmiResult = (weight / height / height) * 10000;
+            }
+            bmiResult = System.Math.Floor(bmiResult);
+        }
+
+        public string GetBMICategory()
+        {
+            if (bmiResult < 18.5)
+                return "Underweight";
+            else if (bmiResult <= 24.9)
+                return "Normal";
+            else if (bmiResult <= 29.9)
+                return "Overweight";
+            else if (bmiResult <= 34.9)
+                return "Obese";
+            return "35.0...";
+        }
+
+        public void OutputResult()
+        {
+            System.Console.WriteLine(SelectedUnit + " " + bmiResult + " " + GetBMICategory());
+        }
 
 
 
-
-}
     }
 }
