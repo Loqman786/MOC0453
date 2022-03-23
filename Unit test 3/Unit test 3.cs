ed1d53876f10e3c37610548d8a1d4cfee01d1d56
Loqman786 <91545135+Loqman786@users.noreply.github.com>
2022-03-23 13:59:54 +0000

    app 3

--- Unit test 3/Unit test 3.cs
+++ Unit test 3/Unit test 3.cs
@@ -1,13 +1,15 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using ConsoleAppProject.App03;
 namespace Unit_test_3
 {
     [TestClass]
     public class UnitTest1
     {
+        StudentGrades studentGrades = new StudentGrades();
         [TestMethod]
         public void TestMethod1()
         {
+
         }
     }
 }
